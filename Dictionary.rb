f24344f4078c69495265a78a58837ffb925b6dec
akshay <akshaykatoch38@gmail.com>
2024-12-06 09:32:23 +0530

    Learn about dictionary topic

--- Dictionary.rb
+++ Dictionary.rb
@@ -1,1 +1,16 @@
 
+hash = {:name => "Rohit", :age => 20, :gender => "male"}
+
+puts hash[:name] = "akshay"
+puts hash[:age] = "14"
+
+hash.each{|key,value| puts "#{key}=>#{value}"}
+
+for i in 0...hash.length
+  puts "#{hash.keys[i]}=>#{hash.values[i]}"
+end
+
+my_hash = Hash.new
+my_hash[:name] = "rohi"
+
+
